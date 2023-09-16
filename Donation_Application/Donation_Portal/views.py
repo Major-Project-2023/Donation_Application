460c225bbe8b701b13fd76be0f2bd0ad04eebe7f
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-09-16 14:01:29 +0530

    test

--- Donation_Application/Donation_Portal/views.py
+++ Donation_Application/Donation_Portal/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "./template/home.html", context=home)
+    return render(request, "template/home.html", context=home)
     # return HttpResponse("<H1>Home</H1>")

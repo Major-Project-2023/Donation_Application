492de13b91bd4c4d281830d98739980586b6ee95
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-09-16 18:15:56 +0530

    home and portal template

--- Donation_Application/Donation_Portal/views.py
+++ Donation_Application/Donation_Portal/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def home(request):
     return render(request, "home.html")
     # return HttpResponse("<H1>Home</H1>")
+
+def portal(request):
+    return render(request, 'paymentportal.html')

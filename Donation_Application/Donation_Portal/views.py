487512a7970e929ed548206556264f5eb8e67d30
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-09-16 23:40:35 +0530

    active linke

--- Donation_Application/Donation_Portal/views.py
+++ Donation_Application/Donation_Portal/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {'navbar':'home'})
 
 def portal(request):
-    return render(request, 'paymentportal.html')
+    return render(request, 'paymentportal.html', {'navbar':'portal'})
 
 def signup(request):
-    return render(request, "signup.html")
+    return render(request, "signup.html", {'navbar':'signup'})
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'navbar':'login'})

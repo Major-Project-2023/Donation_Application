55802a805989e5f1e6c0abbbcb00a93daaa1dd01
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-09-27 17:20:13 +0530

    Getting data from form

--- Donation_Application/Donation_Portal/models.py
+++ Donation_Application/Donation_Portal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class userSignup(models.Model):
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+    phone = models.IntegerField()
+    country = models.CharField(max_length=20)
--- Donation_Application/Donation_Portal/views.py
+++ Donation_Application/Donation_Portal/views.py
@@ -8,6 +8,8 @@ def portal(request):
     return render(request, 'paymentportal.html', {'navbar':'portal'})
 
 def signup(request):
+    if request.method == 'POST':
+        print(request.POST.get('email'),"\n",request.POST.get('password'))
     return render(request, "signup.html", {'navbar':'signup'})
 
 def login(request):

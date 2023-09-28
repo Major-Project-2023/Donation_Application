938ed686b96f2cd6c96a196132171e1c23d4d785
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-09-28 12:01:25 +0530

    changes

--- Donation_Application/Donation_Portal/views.py
+++ Donation_Application/Donation_Portal/views.py
@@ -8,12 +8,7 @@ def portal(request):
     return render(request, 'paymentportal.html', {'navbar':'portal'})
 
 def signup(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        print(f"Email: {email} Password: {password}")
     return render(request, "signup.html", {'navbar':'signup'})
 
 def login(request):
-    print(request.GET)
     return render(request, 'login.html', {'navbar':'login'})

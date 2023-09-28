a786a694fa5630f32ed97eb61c7e9cb8e4accf30
sohamkekre <sohamkekresoham@gmail.com>
2023-09-28 12:38:48 +0530

    changes

--- Donation_Application/Donation_Portal/admin.py
+++ Donation_Application/Donation_Portal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import(
+    userSignup,
 
+)
 # Register your models here.
+@admin.register(userSignup)
+class userSignupModelAdmin(admin.ModelAdmin):
+    list_display = ['id','email','password','phone','country']

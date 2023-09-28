c72e5e32d8aaaf6a5503ea5d2effe2c8018d0a05
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-09-28 12:00:04 +0530

    refresh

--- Donation_Application/Donation_Portal/urls.py
+++ Donation_Application/Donation_Portal/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('portal/', views.portal, name = 'portal'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name = 'signup'),

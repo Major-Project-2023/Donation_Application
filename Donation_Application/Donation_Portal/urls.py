8f51a0eaa57e3f2d68248a3cfac912dd08ef1373
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-09-30 15:32:57 +0530

    changes

--- Donation_Application/Donation_Portal/urls.py
+++ Donation_Application/Donation_Portal/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     # path('signup/', views.signup, name = 'signup'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html',authentication_form=LoginForm),name='login'),
     path('signup/',views.SignupView.as_view(),name='signup'),
+    path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
 ]

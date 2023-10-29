f60f0512fcf8e88e1fadd075e0f22d87dff915f0
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-10-29 09:10:13 +0530

    changes

--- Donation_Application/Donation_Portal/management/commands/startsocket.py
+++ Donation_Application/Donation_Portal/management/commands/startsocket.py
@@ -7,10 +7,8 @@ HEADERSIZE = 10
 
 class Command(BaseCommand):
     help = 'Open a socket in Django'
-    
     # List to keep track of connected client sockets
     connected_clients = []
-
     def handle_client(self, client_socket):
         try:
             while True:
@@ -30,7 +28,6 @@ class Command(BaseCommand):
             self.i+=1
             time.sleep(60)  # Send a message every 60 seconds
             self.msg = f'{self.i} The time is{time.time()}'
-
             pool = Pool.objects.all()
             obj = []
             for pl in pool:
@@ -43,15 +40,11 @@ class Command(BaseCommand):
                 # print(curr)
                 obj.append(curr)                
                 # print(tr.sender,tr.receiver,tr.sender_paypal_email,tr.receiver_paypal_email,tr.amount)
-            
             # Emptying the pool
-            Pool.objects.all().delete()
-            
+            Pool.objects.all().delete()            
             print(f"\n{self.i} The whole object is\n")
             print(str(obj)+'\n')
-
             self.msg = self.msg + "\n" + str(obj)
-
             self.msg = f"{len(self.msg):<{HEADERSIZE}}" + self.msg
             # self.msg += "[<User: soham>, 'NGO2', 'donor1@donor.com'"
             for client_socket in self.connected_clients:
@@ -59,27 +52,21 @@ class Command(BaseCommand):
                     client_socket.send(self.msg.encode())
                 except Exception as e:
                     print(f'Error sending message to client: {e}')
-
     
     def handle(self, *args, **options):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('127.0.0.10', 7777))
         server_socket.listen(5)
-
         print('Server is listening on port 7777')
-
         # Start the thread for sending periodic messages
         periodic_message_thread = threading.Thread(target=self.send_periodic_messages)
         periodic_message_thread.daemon = True
         periodic_message_thread.start()
-
         while True:
             client_socket, addr = server_socket.accept()
             print(f'Accepted connection from {addr}')
-
             # Add the client socket to the list of connected clients
             self.connected_clients.append(client_socket)
-
             # Start a thread to handle the client
             client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
             client_handler.start()

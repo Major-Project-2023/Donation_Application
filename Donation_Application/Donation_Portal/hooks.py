876846baafdc6a7507431955421b4374c068d149
sohamkekre <sohamkekresoham@gmail.com>
2023-10-29 12:44:33 +0530

    css improvement

--- Donation_Application/Donation_Portal/hooks.py
+++ Donation_Application/Donation_Portal/hooks.py
@@ -26,32 +26,3 @@ def webhook(sender, **kwargs):
         transaction.save()
         
             #   \n{first_name}\n{payer_email}\n{receiver_email}\n{ngo_name}\n{mc_currency}\n{payment_type}\n{User}")
-
-    # if ipn_obj.payment_status == ST_PP_COMPLETED:
-    #     mc_gross = ipn_obj.mc_gross 
-    #     payment_date = ipn_obj.payment_date
-    #     payment_status = ipn_obj.payment_status
-    #     first_name = ipn_obj.first_name
-    #     payer_email = ipn_obj.payer_email
-    #     ngo_name = ipn_obj.item_name
-    #     receiver_email = ipn_obj.receiver_email
-    #     mc_currency = ipn_obj.mc_currency
-    #     payment_type = ipn_obj.payment_type
-    #     user = User.objects.get(username=ipn_obj.custom)
-
-        
-
-    #     print(f"{mc_gross}\n{payment_date}\n{payment_status}\n{first_name}\n{payer_email}\n{receiver_email}\n{ngo_name}\n{mc_currency}\n{payment_type}\n{User}")
-    #     transaction = Transaction()
-    #     transaction.sender = user,
-    #     transaction.receiver = ngo_name,
-    #     transaction.sender_paypal_email = payer_email,
-    #     transaction.receiver_paypal_email = receiver_email,
-    #     transaction.date = payment_date,
-    #     transaction.amount = mc_gross,
-    #     transaction.currency = mc_currency,
-    #     transaction.payment_status = payment_status,
-    #     transaction.mode_of_payment = payment_type,
-
-    #     transaction.save()
-

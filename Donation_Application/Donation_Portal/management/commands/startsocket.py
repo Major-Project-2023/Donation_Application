4ce9b8e6436972a29c17f95a15eea8a70ab7e961
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-10-29 11:25:00 +0530

    Changes

--- Donation_Application/Donation_Portal/management/commands/startsocket.py
+++ Donation_Application/Donation_Portal/management/commands/startsocket.py
@@ -15,9 +15,9 @@ class Command(BaseCommand):
                 data = client_socket.recv(1024)
                 if not data:
                     break
-                print(f"Received from {client_socket.getpeername()}: {data.decode()}")
+                self.stdout.write(self.style.SUCCESS(f"Received from {client_socket.getpeername()}: {data.decode()}"))
         except Exception as e:
-            print(f'Error: {e}')
+            self.stdout.write(self.style.WARNING(f'Error: {e}'))
         finally:
             client_socket.close()
             self.connected_clients.remove(client_socket)
@@ -26,7 +26,7 @@ class Command(BaseCommand):
         self.i = 0
         while 1:
             self.i+=1
-            time.sleep(60)  # Send a message every 60 seconds
+            time.sleep(10)  # Send a message every 60 seconds
             self.msg = f'{self.i} The time is{time.time()}'
             pool = Pool.objects.all()
             obj = []
@@ -37,13 +37,11 @@ class Command(BaseCommand):
                 curr.append(pl.sender_paypal_email)
                 curr.append(pl.receiver_paypal_email)
                 curr.append(pl.amount)
-                # print(curr)
                 obj.append(curr)                
-                # print(tr.sender,tr.receiver,tr.sender_paypal_email,tr.receiver_paypal_email,tr.amount)
             # Emptying the pool
             Pool.objects.all().delete()            
-            print(f"\n{self.i} The whole object is\n")
-            print(str(obj)+'\n')
+            self.stdout.write(self.style.HTTP_INFO(f"\n{self.i} The whole object is\n"))
+            self.stdout.write(self.style.HTTP_INFO(str(obj)+'\n'))
             self.msg = self.msg + "\n" + str(obj)
             self.msg = f"{len(self.msg):<{HEADERSIZE}}" + self.msg
             # self.msg += "[<User: soham>, 'NGO2', 'donor1@donor.com'"
@@ -51,20 +49,20 @@ class Command(BaseCommand):
                 try:
                     client_socket.send(self.msg.encode())
                 except Exception as e:
-                    print(f'Error sending message to client: {e}')
+                    self.stdout.write(self.style.WARNING(f'Error sending message to client: {e}'))
     
     def handle(self, *args, **options):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('127.0.0.10', 7777))
         server_socket.listen(5)
-        print('Server is listening on port 7777')
+        self.stdout.write(self.style.WARNING('Socket is listening on port 7777'))
         # Start the thread for sending periodic messages
         periodic_message_thread = threading.Thread(target=self.send_periodic_messages)
         periodic_message_thread.daemon = True
         periodic_message_thread.start()
         while True:
             client_socket, addr = server_socket.accept()
-            print(f'Accepted connection from {addr}')
+            self.stdout.write(self.style.WARNING(f'Accepted connection from {addr}'))
             # Add the client socket to the list of connected clients
             self.connected_clients.append(client_socket)
             # Start a thread to handle the client
